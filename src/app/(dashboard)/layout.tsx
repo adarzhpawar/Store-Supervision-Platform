3d0f1f0024d0d102ee25735459b77a0032c66702
Adarsh Pathade <adarzhpawar@gmail.com>
2026-06-30 01:27:42 +0530

    Fix static generation errors on Vercel

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -2,6 +2,8 @@ import { Sidebar } from "@/components/Sidebar";
 import { requireAuth } from "@/lib/auth";
 import { StoreThemeProvider } from "@/components/StoreThemeProvider";
 
+export const dynamic = "force-dynamic";
+
 export default async function DashboardLayout({
   children,
 }: Readonly<{
